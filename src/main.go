2515c6af7125aa59092b87a517bee99711a2bd42
pwz <273206491@qq.com>
2021-07-15 10:37:05 +0800

    开发环境搭建中增加获取控制台标准输入的文档

--- src/main.go
+++ src/main.go
@@ -5,19 +5,5 @@ import (
 )
 
 func main() {
-	ret := add(0, 2)
-	fmt.Println("程序运行结束", ret)
-}
-
-func add(i, j int) int {
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Println(err)
-		}
-	}()
-	if i < 0 {
-		panic("i值不能小于0")
-	}
-	i = j / i
-	return 1 + j
+	fmt.Println("hello,go")
 }
